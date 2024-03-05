942ffd9e0bea648a259d94bbe5c0e896968797ec
nandemoiikedo <litianr35@gmail.com>
2024-03-05 06:02:32 +0000

    implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcomne to the Sample App!"
-      redirect_to user_url(@user)
-      # 保存の成功をここで扱う。
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
